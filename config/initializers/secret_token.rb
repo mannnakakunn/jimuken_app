e7a1069e84f9535161e474a1b40f5e53c349cb7f
Mannnakakunn <mannnakakunn@gmail.com>
2015-08-18 18:23:11 +0900

    Jimuken initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JimukenApp::Application.config.secret_key_base = 'f8b5d929adccb773792472b9dae9c5136e54c40b8189ae81bb2c59ceb56b1ec1b59ba14be9a18fe1a3c40c6acd00da5419476f02f4a4ea67c844dfa77aaeaabf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JimukenApp::Application.config.session_store :cookie_store, key: '_jimuken_app_session'
